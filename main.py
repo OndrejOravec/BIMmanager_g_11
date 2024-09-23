22ab3fd375a4f133c4a0ea80b62b4977a3ba958c
OndrejOravec <s223260@dtu.dk>
2024-09-23 16:36:34 +0200

    change
    added lines of script pointing into my directory

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #pip install ifcopenshell
 
-#TO UPDATE INFORMATION FROM GROUPS RUN COMMAND:
+#1TO UPDATE INFORMATION FROM GROUPS RUN COMMAND:
 #git submodule update --rebase --remote --recursive
+
+#2. TO RUNNS SCIPT, REMOVE # FROM CORRESPODNING LINE OF SCRIPT POINTING INTO UR DIRECTORY 
 
 import ifcopenshell
 print("Imported ifcopenshell")
@@ -13,7 +15,14 @@ print("Loading model...")
 #model = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_MEP.IFC")
 
 #Architectural model:
-model = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_ARC.IFC")
+#model = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_ARC.IFC")
+print("Model loaded.")
+
+#MEP model Ondrej :
+model = ifcopenshell.open("C:\\Users\\ondro\\Desktop\\skola\\DTU MSc\\3year\\Advanced BIM\\BIM models\\C:\Users\\ondro\\Desktop\\skola\\DTU MSc\\3year\\Advanced BIM\\BIM models\\CES_BLD_24_06_MEP.IFC")
+
+#Architectural model Ondrej:
+#model = ifcopenshell.open("C:\\Users\\ondro\\Desktop\\skola\\DTU MSc\\3year\\Advanced BIM\\BIM models\\C:\\Users\\ondro\\Desktop\\skola\\DTU MSc\\3year\\Advanced BIM\\BIM models\\CES_BLD_24_06_ARC.IFC")
 print("Model loaded.")
 
 #Checking model loading correctly
