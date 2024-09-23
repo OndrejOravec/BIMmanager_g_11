8c761bfe654a1880bd4ba0e83f0c3752820f1822
MagnusRathenborg <s193950@student.dtu.dk>
2024-09-23 17:00:42 +0200

    Updating for hand-in

--- README.md
+++ README.md
@@ -14,18 +14,20 @@
 
 ### 3.1 Group 14 - Claim
 
-- **Claim:** There should be 10 AHU units in total, with 1 unit on each level. The group checks if there is exactly 1 AHU unit per level in the IFC model.
+- **Claim:** There should be 10 AHU units in total, with 1 unit on each level. The group checks how many AHU's there are in the model.
 - **Report Reference:** Claim stated on page 8 of the report "CES_BLD_24_0_6_MEP."
 
 ---
 
 ### 3.2 Group 15 - Claim
 
-- **Claim:** 
-- **Report Reference:** 
+- **Claim:** There should be 1509 windows in the architectural model.
+- **Report Reference:** This claim is not stated in one of the reports, but has been accepted.
 
 ---
 
 ### 4. Our Role as Managers
 
-- **Script or Plan**: Our script is importing Ifcopenshell and running both the Architectural model and the MEP model. We use "#" to toggle each path on or off. Right now we
+- **Script or Plan**: Our script is importing Ifcopenshell and running both the Architectural model and the MEP model. We use "#" to toggle each path on or off. We run the results of their scripts so we aren't running the rules, but the part of the main script that counts number of AHU's and number of windows. After the script it is seen that there are 2 AHU's in the MEP model and 1509 windows in the architectural model.
+Links to group 14 repos: https://github.com/Mathu21DTU/BIManalyst_g_14.git
+Link to group 15 repos: https://github.com/ZKA1104/BIManalyst_g_15.git
--- main.py
+++ main.py
@@ -8,15 +8,15 @@
 import ifcopenshell
 print("Imported ifcopenshell")
 
+from external.BIManalyst_g_14.rules import AHURule
 from external.BIManalyst_g_15.rules import windowRule
 
-
-print("Loading model...")
+print("Loading MEP-model...")
 #MEP model:
-model = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_MEP.IFC")
-
+model_MEP = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_MEP.IFC")
+print("Loading ARC-model...")
 #Architectural model:
-#model = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_ARC.IFC")
+model_ARC = ifcopenshell.open("C:\\Users\\magnu\\OneDrive - Danmarks Tekniske Universitet\\DTU\\Kandidat\\Tredje semester\\41934 Advanced Building Information Modeling\\IFC_Models\\CES_BLD_24_06_ARC.IFC")
 print("Model loaded.")
 
 #MEP model Ondrej :
@@ -26,14 +26,28 @@ print("Model loaded.")
 #model = ifcopenshell.open("C:\\Users\\ondro\\Desktop\\skola\\DTU MSc\\3year\\Advanced BIM\\BIM models\\C:\\Users\\ondro\\Desktop\\skola\\DTU MSc\\3year\\Advanced BIM\\BIM models\\CES_BLD_24_06_ARC.IFC")
 print("Model loaded.")
 
-#Checking model loading correctly
-if model is None:
-    print("Failed to load IFC model.")
+#Checking MEP-model loading correctly
+if model_MEP is None:
+    print("Failed to load MEP model.")
 else:
-    print("IFC model loaded successfully.")
+    print("MEP model loaded successfully.")
+
+
+#Checking ARC-model loading correctly
+if model_ARC is None:
+    print("Failed to load ARC model.")
+else:
+    print("ARC model loaded successfully.")
 
 #Script begins
 print("Script started")
-windowResult = windowRule.checkRule(model)
+
+#Group 14 (MEP-Model)
+AHUResult = AHURule.checkRule(model_MEP)
+print("AHU rule executed")
+print("AHU result:", AHUResult)
+
+#Group 15 (ARC-Model)
+windowResult = windowRule.checkRule(model_ARC)
 print("Window rule executed")
 print("Window result:", windowResult)
